a808e7d2881ba53f91b009752a6168f0f99dda8f
sysraccoon <sysraccoon@gmail.com>
2024-11-18 22:36:03 +0300

    fix project build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,26 @@
 import externals from 'rollup-plugin-node-externals';
+import terser from '@rollup/plugin-terser';
 
-export default [
+/** @type {import('rollup').RollupOptions} */
+const config = [
   {
-    input: "./src/parser.js",
-    output: [{
-      format: "cjs",
-      file: "./dist/index.cjs"
-    }, {
-      format: "es",
-      file: "./dist/index.js"
-    }],
+    input: './src/parser.js',
+    output: {
+      file: './dist/index.min.js',
+      format: 'es',
+    },
+    plugins: [externals(), terser()],
     external: [/^@lezer\/.*/],
+  },
+  {
+    input: './src/parser.js',
+    output: {
+      file: './dist/index.js',
+      format: 'es',
+    },
     plugins: [externals()],
+    external: [/^@lezer\/.*/],
   },
 ];
+
+export default config;
